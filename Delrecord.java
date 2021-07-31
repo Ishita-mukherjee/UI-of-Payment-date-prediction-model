93f6e9b77abc8e91aa8ebb8f3774ed591332afdf
Ishita-mukherjee <ishitamukherjee99@gmail.com>
2021-07-31 10:09:04 +0530

    Add files via upload

---
+++ Delrecord.java
@@ -0,0 +1,60 @@
+package com.higradius;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+/**
+ * Servlet implementation class Delrecord
+ */
+@WebServlet("/Delrecord")
+public class Delrecord extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Delrecord() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String deleteaction = request.getParameter("getdeleteval");
+		String[] arrOfStr = deleteaction.split(",", 11);
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/highradius","root","root"); 
+			Statement st=con.createStatement();
+				for(int i=0;i<arrOfStr.length;i++)
+				{
+					int ind=Integer.parseInt(arrOfStr[i]);
+					String query = "delete from mytable where label="+ind+"";
+					int rs=st.executeUpdate(query);
+				}
+				response.sendRedirect("index.jsp");
+		}
+		catch(Exception e)
+		{
+			response.getWriter().print(e);
+		}
+	}
+	}
+
+
